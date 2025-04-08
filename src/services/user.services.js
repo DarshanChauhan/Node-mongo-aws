104d4acbf630eac734b5b3bdb52d131c1ea62317
Darshan@123 <darshan.chauhan@proexelancers.com>
2025-04-08 11:43:55 +0530

    some minor changes

--- src/services/user.services.js
+++ src/services/user.services.js
@@ -3,7 +3,7 @@ const User = require("../models/user.model");
 const createUser = async (userData) => {
   const existingEmail = await User.findOne({ email: userData.email });
   if (existingEmail) {
-    throw new Error("Email already exists 👽🔴");
+    throw new Error("Email already exists 🚫");
   }
   return await User.create(userData);
 };
