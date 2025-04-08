0f7351a629a09e5b115599fc45aac75dbb6051a6
Darshan@123 <darshan.chauhan@proexelancers.com>
2025-04-08 12:00:26 +0530

    afaf

--- src/services/user.services.js
+++ src/services/user.services.js
@@ -3,7 +3,7 @@ const User = require("../models/user.model");
 const createUser = async (userData) => {
   const existingEmail = await User.findOne({ email: userData.email });
   if (existingEmail) {
-    throw new Error("Email already exists 🚫");
+    throw new Error("Email already exists ❣️");
   }
   return await User.create(userData);
 };
